e484ed32469ec7d250eb37dc18ae204f64dee636
glen15 <glen15@naver.com>
2024-02-05 15:06:13 +0900

    update stduents db auto create code

---
+++ database/students_database_creator.py
@@ -0,0 +1,51 @@
+# 생성된 사용자 정보 예시
+# 데이터베이스 이름 : UNIV_DB_001
+# 유저 이름 : UNIV_001
+# 유저 암호 : 001001
+
+
+import boto3
+import pymysql
+
+# RDS 연결 정보
+rds_host = "AWS RDS 데이터베이스 엔드포인트"
+db_username = 'RDS 사용자'
+db_password = 'RDS 사용자 암호'
+db_name="notes"
+
+# RDS에 연결
+try:
+    conn = pymysql.connections.Connection(host=rds_host, user=db_username, password=db_password, database=db_name)
+except Exception as e:
+    print(f"Error connecting to RDS: {e}")
+    exit()
+
+# 사용자 및 데이터베이스 생성 및 권한 부여
+def create_database_and_user(user_index):
+    # 사용자 이름, 데이터베이스 이름, 비밀번호 생성
+    user_name = f"UNIV_{user_index:03d}"
+    db_name = f"UNIV_DB_{user_index:03d}"
+    user_password = f"{user_index:03d}{user_index:03d}"
+
+    with conn.cursor() as cursor:
+        # 데이터베이스 생성
+        create_db_query = f"CREATE DATABASE IF NOT EXISTS {db_name};"
+        cursor.execute(create_db_query)
+
+        # 사용자 생성 및 권한 부여
+        create_user_query = f"CREATE USER IF NOT EXISTS '{user_name}'@'%' IDENTIFIED BY '{user_password}';"
+        cursor.execute(create_user_query)
+        grant_query = f"GRANT ALL PRIVILEGES ON {db_name}.* TO '{user_name}'@'%';"
+        cursor.execute(grant_query)
+        conn.commit()
+
+# 사용자 및 데이터베이스를 원하는 횟수만큼 생성
+number_of_users = 16  # 사용자 및 데이터베이스를 몇 개 생성할 지 지정
+for i in range(1, number_of_users + 1):
+    create_database_and_user(i)
+
+# RDS 연결 종료
+conn.close()
+
+# pip install boto3 pymysql
+# python3 database_creator.py
