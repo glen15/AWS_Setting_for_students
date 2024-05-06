724244c9d5ed1785af4a26e68242d8a8bc48f90e
Glen <glen15@naver.com>
2024-05-06 11:48:42 +0900

    Update start.sh

--- IAM/start.sh
+++ IAM/start.sh
@@ -2,5 +2,6 @@ npm install -g aws-cdk --force
 python3 -m venv .venv
 source .venv/bin/activate
 pip install aws-cdk-lib constructs
+cdk bootstrap 
 cdk deploy --app "python iam-creator.py"
 # cdk destroy --app "python iam-creater.py"
